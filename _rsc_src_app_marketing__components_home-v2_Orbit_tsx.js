a54fd84d9f8cfd6fd9630289d9dae0b9a5ab2988
ShubhamJatti <61312819+ShubhamMadhukarJatti@users.noreply.github.com>
2026-07-18 15:41:51 +0530

    Add files via upload

---
+++ _rsc_src_app_marketing__components_home-v2_Orbit_tsx.js
@@ -0,0 +1,51 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_src_app_marketing__components_home-v2_Orbit_tsx";
+exports.ids = ["_rsc_src_app_marketing__components_home-v2_Orbit_tsx"];
+exports.modules = {
+
+/***/ "(rsc)/./src/app/(marketing)/_components/home-v2/Orbit.tsx":
+/*!***********************************************************!*\
+  !*** ./src/app/(marketing)/_components/home-v2/Orbit.tsx ***!
+  \***********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"(rsc)/../node_modules/.pnpm/next@14.1.4_@babel+core@7.2_113af866fd2ebb7566d519e3e52fa0ac/node_modules/next/dist/server/future/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"(rsc)/../node_modules/.pnpm/next@14.1.4_@babel+core@7.2_113af866fd2ebb7566d519e3e52fa0ac/node_modules/next/dist/server/future/route-modules/app-page/vendored/rsc/react.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _layout_OrbitLogos__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../layout/OrbitLogos */ \"(rsc)/./src/app/(marketing)/_components/layout/OrbitLogos.tsx\");\n\n\n\nconst Orbit = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_layout_OrbitLogos__WEBPACK_IMPORTED_MODULE_2__.OrbitingCirclesDemo, {}, void 0, false, {\n            fileName: \"C:\\\\Users\\\\shubham jatti\\\\Downloads\\\\shakdom\\\\sharkdom-Frontend-dev\\\\sharkdom-Frontend-dev\\\\src\\\\app\\\\(marketing)\\\\_components\\\\home-v2\\\\Orbit.tsx\",\n            lineNumber: 8,\n            columnNumber: 7\n        }, undefined)\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\shubham jatti\\\\Downloads\\\\shakdom\\\\sharkdom-Frontend-dev\\\\sharkdom-Frontend-dev\\\\src\\\\app\\\\(marketing)\\\\_components\\\\home-v2\\\\Orbit.tsx\",\n        lineNumber: 7,\n        columnNumber: 5\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Orbit);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9zcmMvYXBwLyhtYXJrZXRpbmcpL19jb21wb25lbnRzL2hvbWUtdjIvT3JiaXQudHN4IiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBeUI7QUFFaUM7QUFFMUQsTUFBTUUsUUFBUTtJQUNaLHFCQUNFLDhEQUFDQztrQkFDQyw0RUFBQ0YsbUVBQW1CQTs7Ozs7Ozs7OztBQUcxQjtBQUVBLGlFQUFlQyxLQUFLQSxFQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc2hhcmtkb20uY29tLy4vc3JjL2FwcC8obWFya2V0aW5nKS9fY29tcG9uZW50cy9ob21lLXYyL09yYml0LnRzeD9mZTAzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcblxuaW1wb3J0IHsgT3JiaXRpbmdDaXJjbGVzRGVtbyB9IGZyb20gJy4uL2xheW91dC9PcmJpdExvZ29zJ1xuXG5jb25zdCBPcmJpdCA9ICgpID0+IHtcbiAgcmV0dXJuIChcbiAgICA8ZGl2PlxuICAgICAgPE9yYml0aW5nQ2lyY2xlc0RlbW8gLz5cbiAgICA8L2Rpdj5cbiAgKVxufVxuXG5leHBvcnQgZGVmYXVsdCBPcmJpdFxuIl0sIm5hbWVzIjpbIlJlYWN0IiwiT3JiaXRpbmdDaXJjbGVzRGVtbyIsIk9yYml0IiwiZGl2Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(rsc)/./src/app/(marketing)/_components/home-v2/Orbit.tsx\n");
+
+/***/ }),
+
+/***/ "(rsc)/./src/app/(marketing)/_components/layout/OrbitLogos.tsx":
+/*!***************************************************************!*\
+  !*** ./src/app/(marketing)/_components/layout/OrbitLogos.tsx ***!
+  \***************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   OrbitingCirclesDemo: () => (/* binding */ e0)
+/* harmony export */ });
+/* harmony import */ var next_dist_build_webpack_loaders_next_flight_loader_module_proxy__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! next/dist/build/webpack/loaders/next-flight-loader/module-proxy */ "(rsc)/../node_modules/.pnpm/next@14.1.4_@babel+core@7.2_113af866fd2ebb7566d519e3e52fa0ac/node_modules/next/dist/build/webpack/loaders/next-flight-loader/module-proxy.js");
+
+const proxy = (0,next_dist_build_webpack_loaders_next_flight_loader_module_proxy__WEBPACK_IMPORTED_MODULE_0__.createProxy)(String.raw`C:\Users\shubham jatti\Downloads\shakdom\sharkdom-Frontend-dev\sharkdom-Frontend-dev\src\app\(marketing)\_components\layout\OrbitLogos.tsx`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule, $$typeof } = proxy;
+const __default__ = proxy.default;
+
+const e0 = (0,next_dist_build_webpack_loaders_next_flight_loader_module_proxy__WEBPACK_IMPORTED_MODULE_0__.createProxy)(String.raw`C:\Users\shubham jatti\Downloads\shakdom\sharkdom-Frontend-dev\sharkdom-Frontend-dev\src\app\(marketing)\_components\layout\OrbitLogos.tsx#OrbitingCirclesDemo`);
+
+
+/***/ })
+
+};
+;
